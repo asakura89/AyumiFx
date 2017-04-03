79b85f9911e67b479a15ebd6fc029d99dec61118
asakura89 <flamboyan3no1a@gmail.com>
2017-04-04 00:44:01 +0700

    [Version] change how it versioned

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Arvy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple implementation of return object from a function")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Arvy")]
@@ -32,5 +32,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
