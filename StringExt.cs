f6b1b7a8b5520820b05986bf4f61b111c8638bc3
asakura89 <flamboyan3no1a@gmail.com>
2019-05-08 11:05:14 +0700

    initial commit

---
+++ StringExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Varya {
+    public static class StringExt {
+        public static String ReplaceWith(this String string2Replace, IDictionary<String, String> replacements) {
+            if (replacements != null && replacements.Any()) {
+                return Regex.Replace(string2Replace, "(\\$\\w*)", match => {
+                    if (match.Success) {
+                        if (replacements.ContainsKey(match.Value))
+                            return replacements[match.Value];
+
+                        return match.Value;
+                    }
+
+                    return String.Empty;
+                });
+            }
+
+            return string2Replace;
+        }
+    }
+}
