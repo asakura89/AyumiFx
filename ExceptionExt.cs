2a07d0ea5a8bea310b8a2b1771e0ece3e925d5b6
asakura89 <flamboyan3no1a@gmail.com>
2019-05-08 10:23:06 +0700

    initial commit

---
+++ ExceptionExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Exy {
+    public static class ExceptionExt {
+        public static String GetExceptionMessage(this Exception ex) {
+            var errorList = new StringBuilder();
+            if (ex.InnerException != null)
+                errorList.AppendLine(GetExceptionMessage(ex.InnerException));
+
+            return errorList
+                .AppendLine(ex.Message)
+                .ToString();
+        }
+    }
+}
