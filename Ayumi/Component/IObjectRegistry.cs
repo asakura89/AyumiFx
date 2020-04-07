8b735c08cf714bf4a7ad344d2fdb3baab33436fc
asakura89 <flamboyan3no1a@gmail.com>
2020-04-07 21:33:44 +0700

    [ObjectRegistry] .

--- Ayumi/Component/IObjectRegistry.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Ayumi.Component {
-    public interface IObjectRegistry {
-        A GetRegisteredObject<A>();
-        A GetRegisteredObject<A>(params Object[] constructorParamList);
-        void RegisterObject<A, I>() where I : A;
-    }
-}
--- Ayumi/Component/ObjectRegistry.cs
+++ Ayumi/Component/ObjectRegistry.cs
@@ -1,23 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Ayumi.Component {
     public static class ObjectRegistry {
-        static readonly Dictionary<Type, Type> objectList = new Dictionary<Type, Type>();
+        static readonly IDictionary<Type, Func<Object>> objects = new Dictionary<Type, Func<Object>>();
 
-        public static A GetRegisteredObject<A>() => GetRegisteredObject<A>(null);
+        public static A Get<A>() => (A) GetInstance(typeof(A));
 
-        public static A GetRegisteredObject<A>(params Object[] constructorParamList) {
-            Type implType = objectList[typeof(A)];
-            return (A) Activator.CreateInstance(implType, constructorParamList);
+        public static void Register<A, I>() where I : A => objects[typeof(A)] = () => GetInstance(typeof(I));
+
+        public static void Register<I>(Func<I> instanceCreator) => objects[typeof(I)] = () => instanceCreator();
+
+        public static void Register<I>(I instance) => objects[typeof(I)] = () => instance;
+
+        static Object GetInstance(Type abstractType) {
+            if (objects.TryGetValue(abstractType, out Func<Object> creator))
+                return creator();
+
+            if (!abstractType.IsAbstract)
+                return CreateInstance(abstractType);
+
+            throw new InvalidOperationException($"{abstractType.Name} isn't Registered.");
         }
 
-        public static void RegisterObject<A, I>() where I : A {
-            Type abstractType = typeof(A);
-            Type implType = typeof(I);
+        static Object CreateInstance(Type implType) {
+            ConstructorInfo ctor = implType.GetConstructors()[0];
+            ParameterInfo[] ctorParams = ctor.GetParameters();
+            if (ctorParams.Length == 0)
+                return Activator.CreateInstance(implType);
 
-            if (!objectList.ContainsKey(abstractType))
-                objectList.Add(abstractType, implType);
+            Object[] parameters = ctorParams
+                .Select(param => param.ParameterType)
+                .Select(GetInstance)
+                .ToArray();
+
+            return Activator.CreateInstance(implType, parameters);
         }
     }
 }
