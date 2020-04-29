c398cf8082d92a35fb3664fa8bed1e10274df125
asakura89 <flamboyan3no1a@gmail.com>
2020-04-30 06:00:07 +0700

    [Other] Refactor

--- RyandomNumberGenerator.cs
+++ RyandomNumberGenerator.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace RyaNG {
     public static class RyandomNumberGenerator {
-        public const Int32 Feigenbaum = 46692; // https://en.wikipedia.org/wiki/Feigenbaum_constants
+        // https://en.wikipedia.org/wiki/Feigenbaum_constants
+        public const Int32 Feigenbaum = 46692;
 
         public static Int32 TurnToRyandom(this Int32 lowerBound, Int32 upperBound) {
             Int32 seed = Guid.NewGuid().GetHashCode() % Feigenbaum;
@@ -10,10 +11,6 @@ namespace RyaNG {
             return rnd.Next(lowerBound, upperBound);
         }
 
-        public static Int32 TurnToRyandom(this Int32 upperBound) {
-            Int32 seed = Guid.NewGuid().GetHashCode() % Feigenbaum;
-            var rnd = new Random(seed);
-            return rnd.Next(0, upperBound);
-        }
+        public static Int32 TurnToRyandom(this Int32 upperBound) => 0.TurnToRyandom(upperBound);
     }
 }
