7578ff2560122bf7adf85b01978fadac44f9b0b6
asakura89 <flamboyan3no1a@gmail.com>
2021-05-24 21:15:44 +0700

    [Improved] Update Figenbaum to easy-to-remember one 😅

--- RyandomNumberGenerator.cs
+++ RyandomNumberGenerator.cs
@@ -3,7 +3,7 @@ using System;
 namespace RyaNG {
     public static class RyandomNumberGenerator {
         // https://en.wikipedia.org/wiki/Feigenbaum_constants
-        public const Int32 Feigenbaum = 46692;
+        public const Int32 Feigenbaum = 46692016;
 
         public static Int32 TurnToRyandom(this Int32 lowerBound, Int32 upperBound) {
             Int32 seed = Guid.NewGuid().GetHashCode() % Feigenbaum;
