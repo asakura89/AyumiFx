f6728c05f0e4aad5c6ee81dce3a3ace6b4633e55
asakura89 <flamboyan3no1a@gmail.com>
2021-05-24 22:56:11 +0700

    [Fixed] Versioning and build tools

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2020.0921")]
-[assembly: AssemblyFileVersion("1.0.2020.0921")]
+[assembly: AssemblyVersion("1.0.2021.1424")]
+[assembly: AssemblyFileVersion("1.0.2021.1424")]
